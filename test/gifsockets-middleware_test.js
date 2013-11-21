ba6e4717e987c993050b32534c0087af9f28dc22
Todd Wolfson <todd@twolfson.com>
2013-11-21 00:47:50 -0800

    Dealing with copy/pasta testing

--- test/gifsockets-middleware_test.js
+++ test/gifsockets-middleware_test.js
@@ -1,13 +1,46 @@
 var http = require('http');
 var rawBody = require('raw-body');
+var request = require('request');
+var imageUtils = require('./utils/image');
 var serverUtils = require('./utils/server');
 var GifsocketsMiddleware = require('../');
+
+function openImage() {
+  before(function (done) {
+    var that = this;
+    this.gifData = '';
+    var req = request({
+      url: 'http://localhost:8050/',
+      encoding: 'binary'
+    });
+    req.on('error', function (err) {
+      done(err);
+    });
+    req.on('response', function (res) {
+      res.on('data', function (buff) {
+        that.gifData += buff;
+      });
+      that.gifRes = res;
+      done();
+    });
+  });
+}
+
+function closeImage() {
+  before(function (done) {
+    this.gifRes.on('end', done);
+    request({
+      url: 'http://localhost:8050/',
+      method: 'DELETE'
+    });
+  });
+}
 
 describe('A server using gifsockets-middleware', function () {
   serverUtils.runPixelServer();
   before(function startGifsocketsMiddleware (done) {
     // Create a set of middlewares and server
-    var middlewares = GifsocketsMiddleware({width: 200, height: 200});
+    var middlewares = GifsocketsMiddleware({width: 10, height: 10});
     var app = http.createServer(function (req, res) {
       // DEV: We are intentionally not using express to very it works at any level
       if (req.method === 'GET') {
@@ -26,7 +59,7 @@ describe('A server using gifsockets-middleware', function () {
 
           // Forward the request appropriately
           if (req.method === 'POST') {
-            middlewares.writePixelsToImages(req, res);
+            middlewares.writeJsonToImages(req, res);
           // DEV: This is a catch-all case but iddealy PUT
           } else {
             middlewares.writeTextToImages(req, res);
@@ -44,7 +77,65 @@ describe('A server using gifsockets-middleware', function () {
     this.app.close(done);
   });
 
-  it('', function () {
+  describe('receiving a request', function () {
+    openImage();
+    before(function saveGifData () {
+      this._beforeFrameData = this.gifData;
+    });
+    before(function writeNewFrame (done) {
+      this.timeout(5000);
+      request({
+        url: 'http://localhost:8050/image/text',
+        method: 'POST',
+        form: {
+          text: 'Hello',
+          'font-family': 'Arial',
+          'font-size': '2'
+        }
+      }, done);
+    });
 
+    it('receives a new frame', function () {
+      assert.notEqual(this._beforeFrameData, this.gifData);
+    });
+
+    describe('and closing the image', function () {
+      closeImage();
+      imageUtils.debug('text.gif');
+
+      it('creates a GIF image', function () {
+        // TODO: We might be able to use image magick for a fuzzy match
+        var expectedImages = ['text.gif', 'text2.gif'];
+        var matchedAnImage = false;
+        var i = expectedImages.length;
+        while (i--) {
+          var expectedImg = fs.readFileSync(__dirname + '/expected-files/' + expectedImages[i], 'binary');
+          if (expectedImg === this.gifData) {
+            matchedAnImage = true;
+            break;
+          }
+        }
+        assert(matchedAnImage);
+      });
+    });
+  });
+
+  describe('writing a JSON pixel frame', function () {
+    openImage();
+    imageUtils.loadRgbaImage(__dirname + '/test-files/checkerboard.png');
+    before(function writeJsonFrame (done) {
+      request({
+        url: 'http://localhost:8050/',
+        method: 'POST',
+        body: JSON.stringify([].slice.call(this.rgbaPixels))
+      }, done);
+    });
+    closeImage();
+    imageUtils.debug('json.gif');
+
+    it('creates the image as a GIF', function () {
+      var expectedImg = fs.readFileSync(__dirname + '/expected-files/json.gif', 'binary');
+      assert.strictEqual(this.gifData, expectedImg);
+    });
   });
 });
---
+++ test/utils/image.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var getPixels = require('get-pixels');
+
+exports.loadRgbaImage = function (filename) {
+  before(function loadImage (done) {
+    var that = this;
+    getPixels(__dirname + '/../test-files/' + filename, function (err, pixels) {
+      if (err) {
+        return done(err);
+      }
+      that.rgbaPixels = pixels.data;
+      done();
+    });
+  });
+};
+
+exports.debug = function (filename) {
+  if (process.env.DEBUG_TEST) {
+    before(function saveDebugImage () {
+      try { fs.mkdirSync(__dirname + '/../actual-files/'); } catch (e) {}
+      fs.writeFileSync(__dirname + '/../actual-files/' + filename, this.streamData, 'binary');
+    });
+  }
+};
